5063188fc8d186d99bb835844f9dd40ed781b4b7
Art San <artsan.mcc@gmail.com>
2023-12-05 23:17:57 +0700

    Uikit

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import {
 	authControllerGetSessionInfo,
 	authControllerSignIn,
@@ -7,8 +7,14 @@ import {
 import { useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { UiButton } from '@/shared/ui/ui-button'
+import { UiTextField } from '@/shared/ui/ui-text-field'
+import { UiSelectField } from '@/shared/ui/ui-select-field'
+import { UiLink } from '@/shared/ui/ui-link'
+import { UiSpinner } from '@/shared/ui/ui-spinner'
+import { UiPageSpinner } from '@/shared/ui/ui-page-spinner'
+import { UiHeader } from '@/shared/ui/ui-header'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] }) // шрифты перенесли app.tsx
 
 export default function HomePage() {
 	// 3:04
@@ -26,16 +32,26 @@ export default function HomePage() {
 
 	return (
 		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+			className={`flex min-h-screen flex-col items-center justify-between p-24`}
 		>
 			{data?.email}
-
+			<UiHeader />
 			<UiButton variant="primary">hex</UiButton>
 			<UiButton variant="secondary">hex</UiButton>
 			<UiButton variant="outlined">hex</UiButton>
 			<UiButton disabled variant="primary">
 				Sign Out
 			</UiButton>
+			<UiTextField inputProps={{ placeholder: 'Enter email...' }} />
+			<UiTextField
+				label="Email"
+				inputProps={{ placeholder: 'Enter email...' }}
+			/>
+			<UiTextField error="просто ошибка" />
+			<UiSelectField options={[{ value: '1', label: 'options' }]} />
+			<UiLink href={'/'}>fffffrrrr</UiLink>
+			<UiSpinner className=" text-teal-600 w-20 h-20" />
+			{/* <UiPageSpinner /> */}
 		</main>
 	)
 }
