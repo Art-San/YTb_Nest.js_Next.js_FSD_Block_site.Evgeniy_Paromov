a2763995b36c388e13fea3886dd7a5d50cf4e501
Art San <artsan.mcc@gmail.com>
2023-12-07 20:52:34 +0700

    Переключение блокировки

--- client/src/entities/account/queries.ts
+++ client/src/entities/account/queries.ts
@@ -1,3 +1,31 @@
+// Тоже рабочий вариант
+// import {
+// 	accountControllerGetAccount,
+// 	accountControllerPatchAccount,
+// } from '@/shared/api/generated'
+// import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
+// const accountKey = ['account']
+// export function useAccountQuery() {
+// 	return useQuery({
+// 		queryKey: accountKey,
+// 		queryFn: accountControllerGetAccount,
+// 	})
+// }
+
+// Важная тема повторяет на 4:55:00
+// export function useUpdateAccountMutation() {
+// 	const queryClient = useQueryClient()
+// 	return useMutation({
+// 		mutationFn: accountControllerPatchAccount,
+// 		async onSettled(data) {
+// 			await queryClient.invalidateQueries({ queryKey: accountKey })
+// 		},
+// 	})
+// }
+
+// { queryKey: sessionKey }
+
 import {
 	accountControllerGetAccount,
 	accountControllerPatchAccount,
@@ -25,4 +53,4 @@ export function useUpdateAccountMutation() {
 	})
 }
 
-// { queryKey: sessionKey }
+// // { queryKey: sessionKey }
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,62 +1,26 @@
-import Image from 'next/image'
-// import { Inter } from 'next/font/google'
-import {
-	authControllerGetSessionInfo,
-	authControllerSignIn,
-} from '@/shared/api/generated'
-import { useEffect } from 'react'
-import { useQuery } from '@tanstack/react-query'
 import { UiButton } from '@/shared/ui/ui-button'
 import { UiTextField } from '@/shared/ui/ui-text-field'
 import { UiSelectField } from '@/shared/ui/ui-select-field'
 import { UiLink } from '@/shared/ui/ui-link'
 import { UiSpinner } from '@/shared/ui/ui-spinner'
-import { UiPageSpinner } from '@/shared/ui/ui-page-spinner'
-import { UiHeader } from '@/shared/ui/ui-header'
-import { SignOutButton } from '@/features/auth'
-import { useSessionQuery } from '@/entities/session/queries'
 
-// const inter = Inter({ subsets: ['latin'] }) // шрифты перенесли app.tsx
+import { UiHeader } from '@/shared/ui/ui-header'
+
+import { ToggleBlockingButton } from '@/features/toggle-blocking/ui/toggle-blocking-button'
+import { Profile } from '@/widgets/profile'
 
 export default function HomePage() {
 	// 3:04
 
-	// useQuery для get
-	// useMutation для POST PUT Этот хук предоставляет удобный способ отправки запросов на создание, обновление или удаление данных на сервере.
-	// useMutation() НЕ НАШЕЛ isLoading
-	// const { data } = useQuery({
-	// 	queryKey: ['session'],
-	// 	queryFn: () => authControllerGetSessionInfo(),
-	// })
-
-	const { data } = useSessionQuery()
-
 	return (
-		<main className={`min-h-screen`}>
-			<UiHeader
-				right={
-					<div>
-						{data?.email}
-						<SignOutButton />
-					</div>
-				}
-			/>
-			<UiButton variant="primary">hex</UiButton>
-			<UiButton variant="secondary">hex</UiButton>
-			<UiButton variant="outlined">hex</UiButton>
-			<UiButton disabled variant="primary">
-				Sign Out
-			</UiButton>
-			<UiTextField inputProps={{ placeholder: 'Enter email...' }} />
-			<UiTextField
-				label="Email"
-				inputProps={{ placeholder: 'Enter email...' }}
-			/>
-			<UiTextField error="просто ошибка" />
-			<UiSelectField options={[{ value: '1', label: 'options' }]} />
-			<UiLink href={'/'}>fffffrrrr</UiLink>
-			<UiSpinner className=" text-teal-600 w-20 h-20" />
-			{/* <UiPageSpinner /> */}
-		</main>
+		<div className={`min-h-screen flex flex-col`}>
+			<UiHeader right={<Profile />} />
+			<div className="grid grid-cols-[200px_1fr]">
+				<aside className="px-5 pt-10">
+					<ToggleBlockingButton />
+				</aside>
+				<main>Block List</main>
+			</div>
+		</div>
 	)
 }
